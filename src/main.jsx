82a1f27ad9b5935b42220f8a1592ecee4ce957b9
yingliufilms <yingliufilms@gmail.com>
2023-09-20 21:46:29 -0400

    client side routing, client side form actions, loading and updating data

--- src/main.jsx
+++ src/main.jsx
@@ -2,19 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
-import Root from './routes/root';
+import Root, {
+	loader as rootLoader,
+	action as rootAction
+} from './routes/root';
 import ErrorPage from './error-page';
-import Contact from './routes/contact';
+import Contact, { loader as contactLoader } from './routes/contact';
+import EditContact, { action as editAction } from './routes/edit';
 
 const router = createBrowserRouter([
 	{
 		path: '/',
 		element: <Root />,
 		errorElement: <ErrorPage />,
+		loader: rootLoader,
+		action: rootAction,
 		children: [
 			{
 				path: 'contacts/:contactId',
-				element: <Contact />
+				element: <Contact />,
+				loader: contactLoader
+			},
+			{
+				path: 'contacts/:contactId/edit',
+				element: <EditContact />,
+				loader: contactLoader,
+				action: editAction
 			}
 		]
 	}
--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,5 +1,18 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, Link, useLoaderData, Form, redirect } from 'react-router-dom';
+import { getContacts, createContact } from '../contacts';
+
+export async function action() {
+	const contact = await createContact();
+	return redirect(`/contacts/${contact.id}/edit`);
+}
+
+export async function loader() {
+	const contacts = await getContacts();
+	return { contacts };
+}
+
 export default function Root() {
+	const { contacts } = useLoaderData();
 	return (
 		<>
 			<div id="sidebar">
@@ -16,16 +29,33 @@ export default function Root() {
 						<div id="search-spinner" aria-hidden hidden={true}></div>
 						<div className="sr-only" aria-live="polite"></div>
 					</form>
+					<Form method="post">
+						<button type="submit">New</button>
+					</Form>
 				</div>
 				<nav>
-					<ul>
-						<li>
-							<a href={`/contacts/1`}>Your Name</a>
-						</li>
-						<li>
-							<a href={`/contacts/1`}>Your Friend</a>
-						</li>
-					</ul>
+					{contacts.length ? (
+						<ul>
+							{contacts.map(({ id, first, last, favorite }) => (
+								<li key={id}>
+									<Link to={`contacts/${id}`}>
+										{first || last ? (
+											<>
+												{first} {last}
+											</>
+										) : (
+											<i>No Name</i>
+										)}{' '}
+										{favorite && <span>*</span>}
+									</Link>
+								</li>
+							))}
+						</ul>
+					) : (
+						<p>
+							<i>No Contact</i>
+						</p>
+					)}
 				</nav>
 			</div>
 			<div id="detail">
